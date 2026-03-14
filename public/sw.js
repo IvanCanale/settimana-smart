178481de621ee0673e4645d92415f2ea6af7bfb3
IvanCanalegit <ivancanale15081995@gmail.com>
2026-03-14 17:39:30 +0100

    auto-update PWA

--- public/sw.js
+++ public/sw.js
@@ -2,16 +2,32 @@ const CACHE_NAME = 'settimana-smart-v1';
 const urlsToCache = ['/'];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forza l'attivazione immediata
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) return response;
-      return fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
